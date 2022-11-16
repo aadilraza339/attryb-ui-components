e372bde5ba38f88e4fb6527afc5ddebf53930e0e
thisisabhinay <a8h1nay@gmail.com>
2022-11-16 12:23:11 +0530

    Fixed logical issues in useEffect

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import React, { useState, useEffect, useRef, useCallback } from "react";
-import ProductSpecifications from "./specifications.jsx"
+import ProductSpecifications from "./specifications.jsx";
 import uniqid from "uniqid";
 
 function App() {
-  let [specifications, setSpecifications] = useState([
-    {
-      label: "Brand Name",
-      value: "Enter Brand Name",
-      name: "Enter specification value",
-      _id: uniqid(),
-      active: false,
-      canEditLabel: false,
-      canRemove: false,
-    },
-  ]);
+    let [specifications, setSpecifications] = useState([
+        {
+            label: "Brand Name",
+            value: "Enter Brand Name",
+            name: "Enter specification value",
+            _id: uniqid(),
+            active: false,
+            canEditLabel: false,
+            canRemove: false,
+        },
+    ]);
 
-  return (
-    <div className="App">
-      <ProductSpecifications
-        setSpecifications={setSpecifications}
-        specifications={specifications}
-      />
-    </div>
-  );
+    return (
+        <div className="App">
+            <ProductSpecifications setSpecifications={setSpecifications} specifications={specifications} />
+        </div>
+    );
 }
 
 export default App;
--- src/specifications.jsx
+++ src/specifications.jsx
@@ -1,22 +1,17 @@
-import React, { useState, useEffect, useRef } from "react";
-import {
-    Button,
-    ProductSpecification,
-    Switch,
-    List,
-    ListItem,
-} from "attryb-ui";
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import { Button, ProductSpecification, Switch, List, ListItem } from "attryb-ui";
 import uniqid from "uniqid";
 
 export default function ProductSpecifications({ specifications, setSpecifications }) {
     const [actives, setActives] = useState([]);
+
     const createNewSpecification = () => {
         let inputFieldId = uniqid();
         var newState = [
             ...specifications,
             {
                 _id: inputFieldId,
-                label: "New Specification",
+                label: "New Specification - " + inputFieldId.substring(inputFieldId.length - 4),
                 value: "",
                 name: "New Specification",
                 canRemove: true,
@@ -24,8 +19,10 @@ export default function ProductSpecifications({ specifications, setSpecification
                 active: false,
             },
         ];
+
         setSpecifications(newState);
     };
+
     const removeInputFields = (newItem) => {
         const list = [...specifications];
         let row = list.filter((item) => {
@@ -34,35 +31,53 @@ export default function ProductSpecifications({ specifications, setSpecification
         setSpecifications(row);
     };
 
-    const updateCardsUIState = (dataId = '', state = false) => {
-        const temp = {}
-        specifications.forEach((data) => { (temp[data._id] = false) })
-        if (dataId) {
-            temp[dataId] = state ? state : true
-        }
-        setActives(temp)
-    };
-    useEffect(() => {
-        if (actives.length == Object.keys(specifications).length) return
-        updateCardsUIState()
-    }, [specifications]);
+    const updateCardsUIState = useCallback(
+        (dataId = "", state = false) => {
+            const temp = {};
+
+            specifications.forEach((data) => {
+                temp[data._id] = false;
+            });
+
+            if (dataId) {
+                temp[dataId] = state ? state : true;
+            }
+
+            setActives(temp);
+        },
+        [specifications]
+    );
+
     const stateToggleSpecifiton = (newItem, flag) => {
-    const list = [...specifications];
-    list.filter((item, index) => {
-      if (item?.uid === newItem?.uid) {
-        list[index] = { ...newItem, active: flag };
-      } else {
-        list[index].active = false;
-      }
-    });
-    setSpecifications(list);
-  };
+        const list = [...specifications];
+        list.filter((item, index) => {
+            if (item?.uid === newItem?.uid) {
+                list[index] = { ...newItem, value: flag };
+            } else {
+                list[index].value = false;
+            }
+        });
+        setSpecifications(list);
+    };
+
+    useEffect(() => {
+        console.log({ specifications });
+        console.log({ actives });
+        if (Object.keys(actives ?? {}).length === specifications.length) return;
+        updateCardsUIState();
+    }, [specifications, updateCardsUIState, actives]);
+
+    useEffect(() => {
+        console.log({ actives });
+    }, [actives]);
+
     return (
         <div className="center">
             {specifications.map((item, index) => {
                 const { active } = { ...item };
+
                 return (
-                    <div className="box__input_list__item">
+                    <div className="box__input_list__item" key={item._id}>
                         <ProductSpecification
                             key={uniqid()}
                             data={item}
@@ -70,73 +85,57 @@ export default function ProductSpecifications({ specifications, setSpecification
                             labelMaxChars={0}
                             canEditLabel={true}
                             canRemove={true}
-                            outsideClickCallback={(
-                                data,
-                                isOutside,
-                                event
-                            ) => {
-                                console.log("outsideClick callback")
+                            outsideClickCallback={(data, isOutside, event) => {
+                                console.log("outsideClick callback");
                                 console.log({
                                     outside: { data, isOutside, event },
                                     active: actives,
-                                })
-                                stateToggleSpecifiton(data)
-                                updateCardsUIState()
+                                });
+                                stateToggleSpecifiton(data);
+                                updateCardsUIState();
                             }}
                             onFocus={() => {
-                                if (actives[item._id]) return
-                                console.log("onFocus")
-                                updateCardsUIState(item._id, true)
+                                if (actives[item._id]) return;
+                                console.log("onFocus");
+                                updateCardsUIState(item._id, true);
                             }}
                             onMouseDown={(event) => {
-                                if (actives[item._id]) return
-                                console.log("onMouseDown")
+                                if (actives[item._id]) return;
+                                console.log("onMouseDown");
                                 console.log({
                                     active: actives,
-                                })
-                                updateCardsUIState(item._id, true)
+                                });
+                                updateCardsUIState(item._id, true);
                             }}
-                            removeCallback={(
-                                callbackItem,
-                                flag,
-                                event
-                            ) => {
-                                console.log("onRemove")
+                            removeCallback={(callbackItem, flag, event) => {
+                                console.log("onRemove");
                                 console.log({
                                     RemovedItemArg: callbackItem,
                                     active: actives,
-                                })
-                                const target = event?.target
+                                });
+                                const target = event?.target;
 
-                                const name = target?.getAttribute("name")
+                                const name = target?.getAttribute("name");
                                 console.log("coming");
-                                updateCardsUIState()
-                                removeInputFields(callbackItem)
+                                updateCardsUIState();
+                                removeInputFields(callbackItem);
                                 // if (
                                 //     event?.key === "Tab"
                                 // ) {
                                 // }
                             }}
-                            onKeyDown={(
-                                event
-                            ) => {
-                                console.log("onKeyDown")
-                                if (actives[item._id]) return
-                                const target = event?.target
-                                const name = target?.getAttribute("name")
-                                if (
-                                    event.key === "Tab" &&
-                                    name === "spec-input"
-                                ) {
-                                    updateCardsUIState()
+                            onKeyDown={(event) => {
+                                console.log("onKeyDown");
+                                if (actives[item._id]) return;
+                                const target = event?.target;
+                                const name = target?.getAttribute("name");
+                                if (event.key === "Tab" && name === "spec-input") {
+                                    updateCardsUIState();
                                 }
                             }}
-
                         />
                     </div>
                 );
-
-
             })}
             <div className="add__specification">
                 <Button variant="outline" onClick={createNewSpecification}>
@@ -144,6 +143,5 @@ export default function ProductSpecifications({ specifications, setSpecification
                 </Button>
             </div>
         </div>
-    )
+    );
 }
-
