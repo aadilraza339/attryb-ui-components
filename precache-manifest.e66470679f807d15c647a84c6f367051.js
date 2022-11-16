eafbcfff71fe8512a19d5ee4f94f4e282c1af0b7
aadilraza339 <aadil18@navgurukul.org>
2022-11-16 19:08:04 +0530

    Updates

---
+++ precache-manifest.e66470679f807d15c647a84c6f367051.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6b34f265f4ce4ab2f2a",
+    "url": "/attryb-ui-components/static/css/main.d62535a1.chunk.css"
+  },
+  {
+    "revision": "d6b34f265f4ce4ab2f2a",
+    "url": "/attryb-ui-components/static/js/main.d6b34f26.chunk.js"
+  },
+  {
+    "revision": "7483025c10a0f8f622e0",
+    "url": "/attryb-ui-components/static/js/1.7483025c.chunk.js"
+  },
+  {
+    "revision": "de6f153ca376f6ffec1b",
+    "url": "/attryb-ui-components/static/js/2.de6f153c.chunk.js"
+  },
+  {
+    "revision": "fcd8579fab3c67d0476f",
+    "url": "/attryb-ui-components/static/js/runtime~main.fcd8579f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/attryb-ui-components/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "f98320f31ecb72ba50080d25ee0aca01",
+    "url": "/attryb-ui-components/index.html"
+  }
+];
