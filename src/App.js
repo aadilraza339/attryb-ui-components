0526f488b758df41ebf2e27b067a77502cd642e7
aadilraza339 <aadil18@navgurukul.org>
2022-11-16 19:37:04 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -4,31 +4,26 @@ import React, { useState, useEffect, useRef, useCallback } from "react";
 import ProductSpecifications from "./components/specifications/index.jsx";
 
 import uniqid from "uniqid";
-import { Routes, Route,HashRouter ,BrowserRouter} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import ComponentList from "./components/component-list/index";
 import EditableContent from "./components/editable-content/index";
 import DropDown from "./components/drop-down/index.jsx";
 function App() {
+  
 
-
-  return (
-
-    <div className="App">
-      <BrowserRouter>
-
-      <HashRouter basename="/">
-
+    return (
+     
+        <div className="App">
+          
         <Routes>
-          <Route path="/editable-content" element={<EditableContent />} />
-          <Route path="/specifications" element={<ProductSpecifications />} />
-          <Route path="/drop-down" element={<DropDown />} />
+          <Route path="/editable-content" element={ <EditableContent/> } />
+          <Route path="/specifications" element={  <ProductSpecifications /> } />
+          <Route path="/drop-down" element={  <DropDown/> } />
 
-          <Route path="/" element={<ComponentList />} />
+          <Route path="/" element={  <ComponentList/> } />
         </Routes>
-      </HashRouter>
-    </BrowserRouter>
-    </div>
-  );
+      </div>
+      );
 }
 
 
