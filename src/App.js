f241e78d161769d66d31347d0064691169ff4d04
aadilraza339 <aadil18@navgurukul.org>
2022-11-16 19:15:28 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     return (
      
         <div className="App">
+          lllll
         <Routes>
           <Route path="/editable-content" element={ <EditableContent/> } />
           <Route path="/specifications" element={  <ProductSpecifications /> } />
